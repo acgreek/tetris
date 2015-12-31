6fa6f773e16b355e4c72773d67f4ae12b7b139b8
Anthony Car <agcarat@gmail.com>
2015-12-30 20:47:10 -0500

    refresh only from move, handle rotate of logs pieces

--- tetris.cpp
+++ tetris.cpp
@@ -91,9 +91,9 @@ class Block : public Movable {
         bool done_moving() {
             return dy_ == 0 && dx_ == 0;
         }
-    private:
         int x_;
         int y_;
+    private:
         int dx_;
         int dy_;
 };
@@ -103,9 +103,10 @@ static blist blocks;
 
 class Piece {
     public:
-        Piece() {}
+        Piece():dir_(HORIZONAL) {}
         virtual ~Piece() {}
         virtual void construct() = 0 ;
+        virtual void rotate() = 0;
         virtual void move() {
             blist::iterator eitr = litr_;
             eitr++;
@@ -149,8 +150,20 @@ class Piece {
             return (eitr != std::find_if(sitr_, eitr, [](Block &b)->bool {return b.done_moving();}));
         }
     protected:
+        void unmarkAll() {
+            blist::iterator eitr = litr_;
+            eitr++;
+            std::for_each(sitr_, eitr, [](Block & b) {b.unmark();});
+        }
+        void markAll() {
+            blist::iterator eitr = litr_;
+            eitr++;
+            std::for_each(sitr_, eitr, [](Block & b) {b.unmark();});
+
+        }
         blist::iterator sitr_;
         blist::iterator litr_;
+        enum {HORIZONAL, VERTICAL} dir_;
 };
 
 class LogPiece :public Piece {
@@ -168,8 +181,153 @@ class LogPiece :public Piece {
             litr_ = blocks.end();
             --litr_;
         }
+        virtual void rotate() {
+            unmarkAll();
+            if (dir_ == HORIZONAL) {
+                if (sitr_->y_ > 0 && CLEAR_BLOCK != board[sitr_->x_+1][sitr_->y_-1]){
+                    markAll();
+                    return;
+                }
+                if (sitr_->y_ + 2 > max_y || CLEAR_BLOCK != board[sitr_->x_+1][sitr_->y_+1]) {
+                    markAll();
+                    return;
+                }
+                if (CLEAR_BLOCK != board[sitr_->x_+1][sitr_->y_+2]) {
+                    markAll();
+                    return;
+                }
+                blist::iterator citr = sitr_;
+                citr->x_++;
+                citr->y_--;
+                citr++;citr++;
+                citr->x_--;
+                citr->y_++;
+                citr++;
+                citr->x_-=2;
+                citr->y_+=2;
+                dir_ = VERTICAL;
+            }
+            else {
+                if (sitr_->x_ == 0 || CLEAR_BLOCK != board[sitr_->x_-1][sitr_->y_+1]) {
+                    markAll();
+                    return;
+                }
+                if (sitr_->x_+2 > max_x || CLEAR_BLOCK != board[sitr_->x_+1][sitr_->y_+1]) {
+                    markAll();
+                    return;
+                }
+                if (CLEAR_BLOCK != board[sitr_->x_+2][sitr_->y_+1]) {
+                    markAll();
+                    return;
+                }
+                blist::iterator citr = sitr_;
+                citr->x_--;
+                citr->y_++;
+                citr++;citr++;
+                citr->x_++;
+                citr->y_--;
+                citr++;
+                citr->x_+=2;
+                citr->y_-=2;
+                dir_ = HORIZONAL;
+            }
+            markAll();
+
+        }
+    private:
+
+        int offset_;
+};
+class SquarePiece :public Piece {
+    public:
+        SquarePiece() {
+            offset_ = 0;
+        }
+        virtual void construct() {
+            blocks.push_back(Block(offset_ + 0,0));
+            sitr_ = blocks.end();
+            --sitr_;
+            blocks.push_back(Block(offset_ + 1,0));
+            blocks.push_back(Block(offset_ + 0,1));
+            blocks.push_back(Block(offset_ + 1,1));
+            litr_ = blocks.end();
+            --litr_;
+        }
+        virtual void rotate() {
+        }
     private:
         int offset_;
+};
+class LZPiece :public Piece {
+    public:
+        LZPiece() {
+            offset_ = 0;
+        }
+        virtual void construct() {
+            blocks.push_back(Block(offset_ + 0,1));
+            sitr_ = blocks.end();
+            --sitr_;
+            blocks.push_back(Block(offset_ + 1,1));
+            blocks.push_back(Block(offset_ + 1,0));
+            blocks.push_back(Block(offset_ + 2,0));
+            litr_ = blocks.end();
+            --litr_;
+        }
+        virtual void rotate() {
+        }
+    private:
+        int offset_;
+};
+class ZPiece :public Piece {
+    public:
+        ZPiece() {
+            offset_ = 0;
+        }
+        virtual void construct() {
+            blocks.push_back(Block(offset_ + 0,0));
+            sitr_ = blocks.end();
+            --sitr_;
+            blocks.push_back(Block(offset_ + 1,0));
+            blocks.push_back(Block(offset_ + 1,1));
+            blocks.push_back(Block(offset_ + 2,1));
+            litr_ = blocks.end();
+            --litr_;
+        }
+        virtual void rotate() {
+        }
+    private:
+        int offset_;
+};
+class PieceSelector {
+    public:
+        PieceSelector(): currentPiece_(SQUARE) {}
+        Piece & getNextPiece() {
+            switch(currentPiece_) {
+                case ZED:
+                    currentPiece_ = LZED;
+                    z_.construct();
+                    return z_;
+                case LZED:
+                    currentPiece_ = LZED;
+                    lz_.construct();
+                    return lz_;
+                case SQUARE:
+                    currentPiece_ = LOG;
+                    log_.construct();
+                    return log_;
+                default:
+                case LOG:
+                    currentPiece_ = ZED;
+                    square_.construct();
+                    return square_;
+            }
+        }
+    private:
+        enum {ZED, LZED, LOG, SQUARE} currentPiece_ ;
+        ZPiece z_;
+        LZPiece lz_;
+        SquarePiece square_;
+        LogPiece log_;
 };
 
 int main(int argc, char *argv[]) {
@@ -178,34 +336,42 @@ int main(int argc, char *argv[]) {
     curs_set(FALSE);
     memset(board, ' ', sizeof(board[0][0]) * MAXBOARDW* MAXBOARDH);
     getmaxyx(stdscr, max_y, max_x);
-    LogPiece logPeace;
 
-    logPeace.construct();
     bool done= false;
     int moveDownCount = 100;
     int currentCount = 1;
+    PieceSelector  pieceSelector;
+    Piece & curPiece =pieceSelector.getNextPiece();
     while(!done) {
+        bool needRedraw =false;
         clear();
-        if (currentCount % moveDownCount == 0 )
-            logPeace.move();
+        if (currentCount % moveDownCount == 0 ) {
+            curPiece.move();
+            needRedraw = true;
+        }
         currentCount++;
         if (kbhit()) {
             char c = lgetch();
+            needRedraw = true;
             switch (c){
                 case 'q': done=true;break;
-                case 'a': logPeace.left();break;
-                case 'd': logPeace.right();break;
-                case 's': logPeace.move();break;
+                case 'a': curPiece.left();break;
+                case 'd': curPiece.right();break;
+                case 'z': curPiece.rotate();break;
+                case 'c': curPiece.rotate();break;
+                case 's': curPiece.move();break;
             }
         }
-        std::for_each(blocks.begin(), blocks.end(), [](Block & b) {b.draw();});
-        refresh();
         usleep(delay);
-        if (logPeace.done_moving()) {
-            logPeace = LogPiece();
-            logPeace.construct();
+        if (needRedraw) {
+            std::for_each(blocks.begin(), blocks.end(), [](Block & b) {b.draw();});
+            wnoutrefresh(stdscr);
+            doupdate();
+            if (curPiece.done_moving()) {
+                curPiece =pieceSelector.getNextPiece();
+            }
         }
     }
     endwin();
-}
+};
 
